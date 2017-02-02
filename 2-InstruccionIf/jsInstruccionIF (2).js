4d0dbd4caf220d7a41636370ab41d0f7f2f82ad6
GoblinDrake <santiagoagusvilar@gmail.com>
2017-02-02 12:26:32 -0300

    sexto commit

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,5 +1,16 @@
 function Mostrar()
 {
+	var num
+	num=document.getElementById("edad").value
+	console.log(num)
+	num=parseInt(num)
+	if(num >= 18){
+		alert("Usted es mayor de edad")
+}
+else{
+	alert("Usted no es mayor de edad")
+}
+
 //tomo la edad  
 
 

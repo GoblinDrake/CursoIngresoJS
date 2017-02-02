5e78a0d4b32091b2a14ea5e87a026bde7b3b786a
GoblinDrake <santiagoagusvilar@gmail.com>
2017-02-02 12:16:41 -0300

    quinto commit

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,17 @@
 function Mostrar()
 {
 //tomo la edad  
+	var num 
+	num=document.getElementById("edad").value 
+	console.log(num)
+	num=parseInt(num)
+	if(num == 15){
+				alert("niña Bonita")}
+		
 
+
+
+	
 
 
 }//FIN DE LA FUNCIÓN

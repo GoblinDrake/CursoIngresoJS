93747eab17f570722eb32d5ca077f73d7d6d4f82
GoblinDrake <santiagoagusvilar@gmail.com>
2017-02-02 11:00:59 -0300

    comitt 3

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,17 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var num
+var num1
+num=document.getElementById("numeroUno").value
+num1=document.getElementById("numeroDos").value
+num=parseInt(num)
+num1=parseInt(num1)
+
+var r =num+num1
+alert("La suma es "+r)
+
+
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,22 +3,50 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
-	
+{	var num
+	var num1
+	num=document.getElementById("numeroUno").value
+	num1=document.getElementById("numeroDos").value
+	num=parseInt(num)
+	num1=parseInt(num1)
+	 var r= num+num1
+	 alert("la suma es "+r)
+
 }
 
 function restar()
 {
-	
+	var num
+	var num1
+	num=document.getElementById("numeroUno").value
+	num1=document.getElementById("numeroDos").value
+	num=parseInt(num)
+	num1=parseInt(num1)
+	var r =num-num1
+	alert("la resta es "+r)
 }
 
 function multiplicar()
 { 
-	
+	var num
+	var num1
+	num=document.getElementById("numeroUno").value
+	num1=document.getElementById("numeroDos").value
+	num=parseInt(num)
+	num1=parseInt(num1)
+	var r =num*num1
+	alert("la multiplicación es "+r)
 }
 
 function dividir()
 {
-	
+	var num
+	var num1
+	num=document.getElementById("numeroUno").value
+	num1=document.getElementById("numeroDos").value
+	num=parseInt(num)
+	num1=parseInt(num1)
+	var r =num/num1
+	alert("la división es "+r)
 }
 
